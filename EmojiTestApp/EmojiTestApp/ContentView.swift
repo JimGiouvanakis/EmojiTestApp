9891f0a22f425397f24b8712fe2e58ad31125462
Dimitris Giouvanakis <dimigiou99@gmail.com>
2024-08-14 20:49:24 +0300

    Commit the all the version 1.0

---
+++ EmojiTestApp/EmojiTestApp/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  EmojiTestApp
+//
+//  Created by Dimitris Giouvanakis on 12/6/24.
+
+
+import SwiftUI
+
+
+enum Emoji : String, CaseIterable {
+    case 😇 , 😜 , 🥸 , 🤬
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .🤬
+    
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size : 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text (emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.palette)
+            }
+            .navigationTitle("EmojI Lovers!")
+            .padding()
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
+
---
+++ EmojiTestApp/EmojiTestApp/EmojiTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiTestAppApp.swift
+//  EmojiTestApp
+//
+//  Created by Dimitris Giouvanakis on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
